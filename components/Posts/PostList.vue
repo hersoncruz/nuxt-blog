314753a398708a817ab428af09538ae45788a31f
Herson Cruz <herson@gmail.com>
2020-03-30 23:50:06 -0600

    Module 3, Lesson 34 completed.

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="post-list">
+    <PostPreview
+      id="1"
+      :is-admin="isAdmin"
+      thumbnail="https://www.freeimageslive.com/galleries/transtech/objects/pics/shiftkey00029.jpg"
+      title="Hello there!"
+      previewText="This is my first post!"/>
+    <PostPreview
+      id="2"
+      :is-admin="isAdmin"
+      thumbnail="https://www.freeimageslive.com/galleries/transtech/objects/pics/shiftkey00029.jpg"
+      title="Hello there again!"
+      previewText="This is my second post!"/>
+    <PostPreview
+      id="3"
+      :is-admin="isAdmin"
+      thumbnail="https://www.freeimageslive.com/galleries/transtech/objects/pics/shiftkey00029.jpg"
+      title="Hi!"
+      previewText="This is my third post!"/>
+  </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default {
+  components: {
+    PostPreview
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
