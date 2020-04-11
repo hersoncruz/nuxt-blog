ba66fd15cd8a1aedc3377ffa121b841c246b2ad8
Herson Cruz <herson@multisistemas.com.sv>
2020-04-10 22:53:53 -0600

    Module 8, Lesson 81 completed!

--- store/index.js
+++ store/index.js
@@ -80,6 +80,8 @@ const createStore = () => {
           localStorage.setItem("tokenExpiration", new Date().getTime() + Number.parseInt(result.expiresIn) * 1000);
           Cookie.set('jwt', result.idToken);
           Cookie.set('expirationDate', new Date().getTime() + Number.parseInt(result.expiresIn) * 1000);
+
+          return this.$axios.$post('http://localhost:3000/api/track-data', { data: "Authenticated!" });
           //vuexContext.dispatch('setLogoutTimer', result.expiresIn * 1000)
           //console.log("TOKEN: " + this.getters.isAuthenticated);
         }).catch(e => console.log(e));
